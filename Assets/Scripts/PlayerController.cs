02c5581e878a0d90dd39a5dbdf844b19fd39bce8
Tristan <t.jack.mac@gmail.com>
2023-08-22 21:37:50 +1200

    Added basic player movement without animation.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
+    private Vector2 input;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+       // if(!(input.x == 0 && input.y == 0))
+     //   {
+      //      isMoving = true;
+     //       animator.SetFloat("moveX", input.x);
+     //       animator.SetFloat("moveY", input.y);
+    //    }
+  //      else
+   //     {
+   //         isMoving = false;
+   //     }
+
+        Vector2 position = transform.position;
+        Vector2 targetPos;
+        targetPos.x = position.x + moveSpeed * input.x * Time.deltaTime;
+        targetPos.y = position.y + moveSpeed * input.y * Time.deltaTime;
+
+    //    if(isWalkable(targetPos))
+ //       {
+         position.x = targetPos.x;
+         position.y = targetPos.y;
+         transform.position = position;
+ //       }
+
+     /*   animator.SetBool("isMoving", isMoving);
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Interact();
+        } */
+    }
+}
