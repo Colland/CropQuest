f584a7fe58054af799823fc781c96920da243a58
lahndrick <lahndrickhendricks2@gmail.com>
2023-09-05 14:54:33 +1200

    plant growth delay

---
+++ Assets/Scripts/Planting.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class Planting : MonoBehaviour
+{
+    public GameObject playerObj;
+    public GameObject plantObj;
+    public GameObject growObj;
+    public LayerMask plantsCollisionLayer;
+    public LayerMask solidObjectsLayer;
+    public int growDelay;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 location = new((int)MathF.Round(playerObj.transform.position.x + 1), (int)MathF.Round(playerObj.transform.position.y), (int)MathF.Round(playerObj.transform.position.z));
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (Physics2D.OverlapCircle(location, 0.2f, plantsCollisionLayer) == null && Physics2D.OverlapCircle(location, 0.2f, solidObjectsLayer) == null)
+            {
+                SpawnPlant(location);
+            }
+
+        }
+    }
+
+    //instantiate the first phase plant and destroy it after set time
+    //coroutine running same time to instantiate second phase
+    void SpawnPlant(Vector3 loc)
+    {
+        Destroy(Instantiate(plantObj, loc, Quaternion.identity), growDelay);
+        StartCoroutine(Grow(loc));
+    }
+
+    //this runs when first phase is started, instantiate after first phase destroyed
+    private IEnumerator Grow(Vector3 loc)
+    {
+        yield return new WaitForSeconds(growDelay);
+        Instantiate(growObj, loc, Quaternion.identity);
+    }
+}
