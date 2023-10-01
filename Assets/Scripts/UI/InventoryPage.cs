53220b54878dae2c1bb1ea6665123c6e82ff97c7
Colland <t.jack.mac@gmail.com>
2023-10-01 15:59:26 +1300

    Set up some inventory scripts
    Can press I to open and close inventory now, also set up functions to instantiate item slots.

---
+++ Assets/Scripts/UI/InventoryPage.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryPage : MonoBehaviour
+{
+    [SerializeField]
+    private InventoryItem itemPrefab;
+
+    [SerializeField]
+    private RectTransform contentPanel;
+
+    List<InventoryItem> listOfItems = new List<InventoryItem>();
+
+    public void InitializeInventoryUI(int inventorySize)
+    {
+        for(int i = 0; i < inventorySize; i++)
+        {
+            InventoryItem item = Instantiate(itemPrefab, Vector3.zero, Quaternion.identity);
+            item.transform.SetParent(contentPanel);
+            listOfItems.Add(item);
+        }
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
