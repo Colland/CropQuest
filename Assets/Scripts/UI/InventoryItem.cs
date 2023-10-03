7432948770162be23496fb38ea38a77a9fb83af3
Colland <t.jack.mac@gmail.com>
2023-10-03 20:25:50 +1300

    Set up inventory events
    Set up the events for selecting and deselecting slots, also dragging and dropping. Events don't do anything yet

--- Assets/Scripts/UI/InventoryItem.cs
+++ Assets/Scripts/UI/InventoryItem.cs
@@ -1,18 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+using System;
 
 public class InventoryItem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Image itemImage;
+    [SerializeField]
+    private TMP_Text quantityTxt;
+    [SerializeField]
+    private Sprite borderImage;
+    [SerializeField]
+    private Sprite SelectedBorderImage;
+
+    public event Action<InventoryItem> OnItemClicked, OnItemDroppedOn,
+                OnItemBeginDrag, OnItemEndDrag, OnRightMouseBtnClick;
+
+    private bool empty = true;
+
+    public void Awake()
     {
-        
+        ResetData();
+        Deselect();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ResetData()
     {
-        
+        this.itemImage.gameObject.SetActive(false);
+        empty = true;
+    }
+
+    public void Deselect()
+    {
+        var test = this.GetComponent<Image>();
+        test.sprite = borderImage;
+    }
+
+    public void SetData(Sprite sprite, int quantity)
+    {
+        this.itemImage.gameObject.SetActive(true);
+        this.itemImage.sprite = sprite;
+        this.quantityTxt.text = quantity + "";
+        empty = false;
+    }
+
+    public void Select()
+    {
+        var test = this.GetComponent<Image>();
+        test.sprite = SelectedBorderImage;
+    }
+
+    public void OnBeginDrag()
+    {
+        if(empty)
+            return;
+        OnItemBeginDrag?.Invoke(this);
+    }
+
+    public void OnDrop()
+    {
+        OnItemDroppedOn?.Invoke(this);
+    }
+
+    public void OnEndDrag()
+    {
+        OnItemEndDrag?.Invoke(this);
+    }
+
+    public void OnPointerClick(BaseEventData data)
+    {
+        if(empty)
+            return;
+        PointerEventData pointerData = (PointerEventData)data;
+        if(pointerData.button == PointerEventData.InputButton.Right)
+        {
+            OnRightMouseBtnClick?.Invoke(this);
+        }
+        else
+        {
+            OnItemClicked?.Invoke(this);
+        }
     }
 }
--- Assets/Scripts/UI/InventoryPage.cs
+++ Assets/Scripts/UI/InventoryPage.cs
@@ -19,7 +19,37 @@ public class InventoryPage : MonoBehaviour
             InventoryItem item = Instantiate(itemPrefab, Vector3.zero, Quaternion.identity);
             item.transform.SetParent(contentPanel);
             listOfItems.Add(item);
+            item.OnItemClicked += HandleItemSelection;
+            item.OnItemBeginDrag += HandleBeginDrag;
+            item.OnItemDroppedOn += HandleSwap;
+            item.OnItemEndDrag += HandleEndDrag;
+            item.OnRightMouseBtnClick += HandleShowItemActions;
         }
+    }
+
+    public void HandleItemSelection(InventoryItem obj)
+    {
+        Debug.Log(obj.name);
+    }
+
+    public void HandleBeginDrag(InventoryItem obj)
+    {
+        
+    }
+
+    public void HandleSwap(InventoryItem obj)
+    {
+        
+    }
+
+    public void HandleEndDrag(InventoryItem obj)
+    {
+        
+    }
+
+    public void HandleShowItemActions(InventoryItem obj)
+    {
+        
     }
 
     public void Show()
