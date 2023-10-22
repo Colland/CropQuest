1436f021b0ab71ad52b22c0c792b8790752b0df4
DeepitChawla <83631383+ILikeDragonsLol@users.noreply.github.com>
2023-10-22 19:27:12 +1300

    PlantFetcher
    Made a PlantFetcher.cs. The plan is to replace the plant objects (plantGrown, plantObj) within Planting.cs for the required plant.
    
    Which will mean that PumpkinPlant.cs is essentially a plant which only contains data for the plant. I will also need a Harvester class of some sorts to sort plants into the inventory, along with their data.

---
+++ Assets/Scripts/Crops/PlantFetcher.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantFetcher : MonoBehaviour
+{
+    public GameObject fetchPlantGrow;
+    public GameObject fetchPlantObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public GameObject fetchGrow()
+    {
+        
+    }
+}
