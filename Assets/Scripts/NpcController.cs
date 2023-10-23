db5e9c2783d04eb37debf6177de5bab3fa938121
Colland <t.jack.mac@gmail.com>
2023-10-23 20:18:13 +1300

    Overhauled the dialogue system.
    Moved the dialogue functions to a DialogueController script. Now each NPC just passes their dialogue and the quest they give to the DialogueController.

--- Assets/Scripts/NpcController.cs
+++ Assets/Scripts/NpcController.cs
@@ -6,79 +6,13 @@ using TMPro;
 
 public class NpcController : MonoBehaviour, Interactable
 {
-    public GameObject Gold;
-    public GameObject XPSystem;
-    public GameObject playerguiPanel;
-    public Questgiver questGiver;
-    public GameObject dialoguePanel;
-    public TextMeshProUGUI dialogueText;
+    public DialogueController dialogueController;
     public string[] dialogue;
-    private int index;
-
-    public float wordSpeed;
-    public GameObject contButton;
-    private Coroutine typingRoutine;
-    public GameObject item;
-
-    private Player player;
+    public Questgiver quest;
 
     public void Interact()
     {
-        if (dialoguePanel.activeInHierarchy)
-        {
-            zeroText();
-        }
-        else
-        {
-            dialoguePanel.SetActive(true);
-            playerguiPanel.SetActive(false);
-            Gold.SetActive(false);
-            XPSystem.SetActive(false);
-            typingRoutine = StartCoroutine(Typing());
-            InteractWithPlayer();
-        }
-    }
-
-    public void NextLine()
-    {
-        contButton.SetActive(false);
-
-        if (index < dialogue.Length - 1)
-        {
-            index++;
-            dialogueText.text = "";
-            typingRoutine = StartCoroutine(Typing());
-        }
-        else
-        {
-            zeroText();
-            questGiver.OpenQuestPopup();
-        }
-    }
-
-    public void zeroText()
-    {
-        StopCoroutine(typingRoutine);
-        dialogueText.text = "";
-        index = 0;
-        dialoguePanel.SetActive(false);
-        playerguiPanel.SetActive(true);
-        Gold.SetActive(true);   
-        XPSystem.SetActive(true);       
-    }
-
-    IEnumerator Typing()
-    {
-        foreach (char letter in dialogue[index].ToCharArray())
-        {
-            dialogueText.text += letter;
-            yield return new WaitForSeconds(wordSpeed);
-        }
-
-        if (dialogueText.text == dialogue[index])
-        {
-            contButton.SetActive(true);
-        }
+        dialogueController.startInteraction(dialogue, quest);
     }
 
     public void InteractWithPlayer()
